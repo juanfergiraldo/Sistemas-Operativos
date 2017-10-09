dd7cd8a2f64e1dc2fe9f6d907674f58748cee7f2
Juan Fernando <juanfer@pc-83.udea.edu.co>
2017-10-09 07:56:50 -0500

    Inicio taller, Lab4.

---
+++ Labs/Lab4/ejer1sec.c
@@ -0,0 +1,12 @@
+/*Se requiere un programa que reciba un vector de números a través de un archivo
+de texto. La idea es que el programa sume todos los números del vector.
+Implemente el programa de dos maneras, la primera de una forma estrictamente secuencial.
+La segunda forma es creando dos hilos, de manera que cada uno de ellos realice
+la sumatoria de la mitad de los componentes del vector. El hilo 1 sumará los primeros
+datos del vector y el hilo 2 los últimos. Luego cuando los dos hilos finalicen muestre en pantalla el resultado.*/
+
+#include <stdio.h>
+
+int main(){
+
+}
